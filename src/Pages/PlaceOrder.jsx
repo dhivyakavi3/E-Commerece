67027944f4e42fcb35b4d39db046009f9bcb020b
dhivyakavi3 <dhivya3597@gmail.com>
2025-11-27 14:45:56 +0530

    placeorder

--- src/Pages/PlaceOrder.jsx
+++ src/Pages/PlaceOrder.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useCart } from "../context/CartContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./PlaceOrder.css";
+import "./Placeorder.css";
 
 const PlaceOrder = () => {
   const { cartItems, clearCart } = useCart();
